fbb889de9513d8460737637a4b04859a32536ffb
John Carney <john@carney.id.au>
2017-03-01 10:44:47 +1100

    Add a code comment

--- lib/fossicker.rb
+++ lib/fossicker.rb
@@ -4,6 +4,8 @@ module Fossicker
   def self.fossick(object, *keys, **options, &block)
     default = options.key?(:default) ? [ options[:default] ] : []
     keys.reduce(object) do |result, key|
+      # We're taking advantage fof the fact that returning from a bloc exits the enclosing method. We invoke fetch
+      # again to ensure we behave the same way with respect to defaults that Array#fetch and Hash#fetch do.
       result.fetch(key) { return result.fetch(key, *default, &block) }
     end
   end
